010d3f94b6090dcfe8a99de602e5599d5e015701
Eyal Toledano <eyaltoledano@me.com>
2019-03-24 00:25:24 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,5 @@
 module Findable
-  module ClassMethods
-    def self.find_by_name(name) # to extend
-      @@songs.detect{|a| a.name == name}
-    end
-  end
-  module InstanceMethods
+  def self.find_by_name(name) # to extend
+    @@songs.detect{|a| a.name == name}
   end
 end
